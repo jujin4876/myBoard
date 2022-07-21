4065c1ac73e3c1fc1f5b8e47acdefd4ad4c47077
DESKTOP-BCD81FB\USER <jujin4876@naver.com>
2022-07-21 16:31:29 +0900

    회원 엔티티 생성 및 회원 가입테스트

---
+++ src/main/java/boradexample/myboard/myboard/domain/member/BaseTimeEntity.java
@@ -0,0 +1,26 @@
+package boradexample.myboard.myboard.domain.member;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    @Column(updatable = true)
+    private LocalDateTime lastModifiedDate;
+
+}
---
+++ src/main/java/boradexample/myboard/myboard/domain/member/Role.java
@@ -0,0 +1,5 @@
+package boradexample.myboard.myboard.domain.member;
+
+public enum Role {
+    User,Admin
+}
